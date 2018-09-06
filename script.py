8ad98dc399201af21d5856b5efff1467fa3a8f38
trudo <raphaelhardy@sfr.fr>
2018-09-06 03:49:22 +0200

    Every family object stores spp objects. No dicts.

--- script.py
+++ script.py
@@ -4,11 +4,12 @@
 import csv
 from yattag import Doc
 
-# On commencera par creer toutes les familles au fur et a mesure
-# Que les spp defilent, en ajoutant ces derniers petit a petit aux
-# listes .spp des familles
-# On triera ensuite cette liste avec spp.sort(key=lambda x: x.latin_name)
-# On rangera les objects 
+# DOTO:
+# - URLs/LINKS
+#    Every family has a directory where their spp pages are stored
+# - HTML
+#    Families have their own webpages with a list of spp
+# - Put all generated website in a different directory 'website/'
 
 class Family(object):
     def __init__(self, name):
@@ -16,6 +17,11 @@ class Family(object):
         self.url = self.name + '.html'
         # Will store species
         self.spp = []
+
+#     def __repr__(self, name):
+#         return self.name
+#     def __str__(self, name):
+#         return self.name
 
 class Plant(object):
     def __init__(self, fr, lat, fam, desc, tags):
@@ -39,95 +45,98 @@ class Plant(object):
         # Generate per-plant page url suffix. Deal with   -Spaces-      -Dots (ssp.)-
         self.url = 'plantlist/' + self.latin_name.replace(' ', '_').replace('.', '') + '.html'
 
-# Create a dictionnary with all plant-objects
-plants = {}
+
+families = []
+
 with open('plants.csv', 'r') as f:
     lists = csv.reader(f)
     for row in lists:
-        # Object name is latin name
-        objectname = row[1]
-        # Create the object
-        plants[objectname] = Plant( row[0], row[1], row[2], row[3], row[4] )
+        family = row[2].split(" ")[0]
+        # Create family if not already created
+        if family not in [f.name for f in families]:
+            families.append(Family(family))
+        for i in families:
+            if i.name == family:
+                families[families.index(i)].spp.append(Plant(row[0],
+                                                           row[1],
+                                                           row[2],
+                                                           row[3],
+                                                           row[4],))
+# Sorting families alphabetically
+families.sort(key=lambda x: x.name)
+# Sorting spp inside families alphabetically
+for f in families:
+    f.spp.sort(key=lambda x: x.latin_name)
+    
+# Create a list containing aliases of every spp
+# allspp = sorted([s for s in [f.spp for f in families]]) # Does not work
+allspp = []
+for f in families:
+    for s in f.spp:
+        allspp.append(s)
+allspp.sort(key=lambda x: x.latin_name)
 
-
-
-
-families = {}
-for i in plants:
-    plant = plants[i]
-    # Stick to the the first family word. Filter old family names, synonyms
-    # or any non-phylogenetic classification.
-    family = plant.family.split(" ")[0]
-    if family not in families:
-        families[family] = Family(family)
-    families[family].spp.append(plant.latin_name)
-# Sort species in families
-for family in families:
-    families[family].spp.sort()
-# Create a sorted list of all existing families
-sortedfamilies = sorted([x for x in families])
-
-def createpage():
-    doc, tag, text, line = Doc().ttl()
-    doc.asis("<!DOCTYPE html>")
-    with tag('html'):
-        with tag('head'):
-            line('title', 'Bonsoir')
-            doc.asis('<meta charset="utf-8">') # Important
-        
-        with tag('body'):
-            line('h1', 'Bienvenue sur mon petit site de merde')
-            with tag('ul'):
-                for i in plants:
-                    plant = plants[i]
-                    with tag('li'):
-                        doc.asis('<em>%s</em> - %s ' %(plant.latin_name,
-                                                      plant.french_name))
-                        # Hyperlink to per-plant page
-                        with tag('a', href=plant.url):
-                            doc.stag('img', src='img/freccia.png')
-
-            with tag('div'):
-                with tag('p'):
-                    line('h2', 'Voici une liste des familles:') 
-                    with tag('ul'):
-                        for family in sortedfamilies:
-                            line('li', families[family].name)
-                            with tag('ul'):
-                                for i in families[family].spp:
-                                    line('li', i)
-
-    return doc.getvalue()
-
-
-with open("index.html", "w") as f:
-    f.write(createpage())
-
-
-# Function to generate per-plant pages
-def plantpage(plantobject):
-    doc, tag, text, line = Doc().ttl()
-    doc.asis("<!DOCTYPE html>")
-    with tag('html'):
-        with tag('head'):
-            line('title', plantobject.latin_name)
-            doc.asis('<meta charset="utf-8">') # Important
-
-        with tag('body'):
-            line('h1', plantobject.latin_name)
-            line('h2', plantobject.french_name)
-            line('h3', plantobject.family)
-            with tag('p'):
-                text(plantobject.description)
-            if plantobject.tags:
-                with tag('ul'): 
-                    for planttag in plantobject.tags:
-                        line('li', planttag)
-
-    return doc.getvalue()
-
-# Write all per-plant pages
-for i in plants:
-    plant = plants[i]
-    with open(plant.url, 'w') as file:
-        file.write(plantpage(plant))
+# def createpage():
+#     doc, tag, text, line = Doc().ttl()
+#     doc.asis("<!DOCTYPE html>")
+#     with tag('html'):
+#         with tag('head'):
+#             line('title', 'Bonsoir')
+#             doc.asis('<meta charset="utf-8">') # Important
+#         
+#         with tag('body'):
+#             line('h1', 'Bienvenue sur mon petit site de merde')
+#             with tag('ul'):
+#                 for i in plants:
+#                     plant = plants[i]
+#                     with tag('li'):
+#                         doc.asis('<em>%s</em> - %s ' %(plant.latin_name,
+#                                                       plant.french_name))
+#                         # Hyperlink to per-plant page
+#                         with tag('a', href=plant.url):
+#                             doc.stag('img', src='img/freccia.png')
+# 
+#             with tag('div'):
+#                 with tag('p'):
+#                     line('h2', 'Voici une liste des familles:') 
+#                     with tag('ul'):
+#                         for family in sortedfamilies:
+#                             line('li', families[family].name)
+#                             with tag('ul'):
+#                                 for i in families[family].spp:
+#                                     line('li', i)
+# 
+#     return doc.getvalue()
+# 
+# 
+# with open("index.html", "w") as f:
+#     f.write(createpage())
+# 
+# 
+# # Function to generate per-plant pages
+# def plantpage(plantobject):
+#     doc, tag, text, line = Doc().ttl()
+#     doc.asis("<!DOCTYPE html>")
+#     with tag('html'):
+#         with tag('head'):
+#             line('title', plantobject.latin_name)
+#             doc.asis('<meta charset="utf-8">') # Important
+# 
+#         with tag('body'):
+#             line('h1', plantobject.latin_name)
+#             line('h2', plantobject.french_name)
+#             line('h3', plantobject.family)
+#             with tag('p'):
+#                 text(plantobject.description)
+#             if plantobject.tags:
+#                 with tag('ul'): 
+#                     for planttag in plantobject.tags:
+#                         line('li', planttag)
+# 
+#     return doc.getvalue()
+# 
+# # Write all per-plant pages
+# for i in plants:
+#     plant = plants[i]
+#     with open(plant.url, 'w') as file:
+#         file.write(plantpage(plant))
